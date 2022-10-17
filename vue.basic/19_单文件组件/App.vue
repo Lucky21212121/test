4aa397b6c0a486e71acf4ee573a7274c2967c31f
Lucky <404182882@qq.com>
2022-10-17 17:48:52 +0800

    上传所学内容:单文件组件

---
+++ vue.basic/19_单文件组件/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- 组件的结构 -->
+    <div>
+        <School></School>
+        <Student></Student>
+    </div>
+    
+</template>
+
+<script>
+// 组件交互相关的代码（数据、方法等等）
+
+// 引入组件
+import School from './School.vue'
+import Student from './Student.vue'
+
+
+export default {
+      name:'App',
+      components :{
+        School,
+        Student
+      }
+      
+    
+    };
+    
+</script>
+
+<style>
+/* 组件的样式 */
+
+</style>
---
+++ vue.basic/19_单文件组件/School.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+          <h2>学校名称:{{schoolName}}</h2>
+          <h2>学校地址:{{address}}</h2>
+          <button @click="showNmae">点击提示学校名</button>
+        </div>
+</template>
+
+<script>
+// 组件交互相关的代码（数据、方法等等）
+
+export default {
+      name:'School',
+      data() {
+        return {
+          schoolName: "尚硅谷",
+          address: "北京昌平",
+        };
+      },
+      methods: {
+        showNmae() {
+          alert(this.schoolName);
+        },
+      },
+    
+    };
+    
+</script>
+
+<style>
+/* 组件的样式 */
+.demo {
+    background-color: orange;
+}
+</style>
