9524f3e10cc94edcc985ca5e6b8ce6ce4a6bee70
Lucky <404182882@qq.com>
2022-12-20 15:04:43 +0800

    返回顶部

--- jianshu/src/pages/home/index.js
+++ jianshu/src/pages/home/index.js
@@ -4,15 +4,20 @@ import Topic from './components/Topic';
 import List from './components/List';
 import Recommend from './components/Recommend';
 import Writter from './components/Writter';
-import {actionCreators} from "./store"
+import {actionCreators} from "./store";
+
 
 
 import {
     HomeWrapper,
     HomeLeft,
     HomeRight,
+    BackTop
 } from './style';
 class Home extends Component {
+    handleScrollTop(){
+        window.scrollTo(0,0);
+    }
     render(){
         return (
             <HomeWrapper>
@@ -25,18 +30,38 @@ class Home extends Component {
                     <Recommend/>
                     <Writter/>
                 </HomeRight>
+                {this.props.showScroll ? <BackTop onClick={this.handleScrollTop}>顶部</BackTop> : null }
             </HomeWrapper>
         )
     }
     componentDidMount(){
-        this.props.changeHomeData()
+        this.props.changeHomeData();
+        this.bindEvents();
+    }
+    componentWillUnmount(){
+        window.removeEventListener('scroll',this.props.changeScrollTopShow)
+    }
+    bindEvents(){
+        window.addEventListener('scroll',this.props.changeScrollTopShow)
     }
 }
+const mapState=(state)=>({
+    showScroll:state.getIn(['home','showScroll'])
+})
 const mapDispatch = (dispatch)=>({
     changeHomeData(){
         const action = actionCreators.getHomeInfo()
-        dispatch(action)
-       
+        dispatch(action)       
+    },
+    changeScrollTopShow(){
+        if(document.documentElement.scrollTop > 100){
+            dispatch(actionCreators.toggleTopShow(true))
+
+        }else {
+            dispatch(actionCreators.toggleTopShow(false))
+        }
+        
+
     }
 })
-export  default connect(null,mapDispatch)(Home)
+export  default connect(mapState,mapDispatch)(Home)
